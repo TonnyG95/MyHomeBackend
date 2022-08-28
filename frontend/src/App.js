edf19ccdb74a4ac70f211b68dc322452ec1cfbdf
TonnyG95 <galic_antonio@hotmail.com>
2022-08-28 20:40:27 +0000

    started on building UI for homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,19 @@ import './App.css';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Listings from './Components/Listings';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <BrowserRouter>
+    <Nav />
     <Routes>
       <Route path='/' element={ <Home /> } />
       <Route path='/login' element={ <Login />} />
       <Route path='/listings' element={ <Listings />} />
     </Routes>
     </BrowserRouter>
+    
   );
 }
 
---
+++ frontend/src/Components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function Card() {
+  return (
+    <Container>Card</Container>
+  )
+}
+
+export default Card
