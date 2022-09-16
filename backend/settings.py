2c5d6be2b69729a37f7f5c9910d3ea718f493e03
TonnyG95 <galic_antonio@hotmail.com>
2022-09-16 13:08:08 +0000

    depolyument

--- backend/settings.py
+++ backend/settings.py
@@ -155,11 +155,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static' )]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 ## STATICFILES_DIRS = 
 
